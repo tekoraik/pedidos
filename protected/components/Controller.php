734d07d31c282b8d70802b09653263013a14a52e
Teko Raik <tekoraik@tekoraik-hp.(none)>
2013-10-01 19:15:49 +0200

    Second

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Controller is the customized base controller class.
+ * Controller is the customized base controller class
  * All controller classes for this application should extend from this base class.
  */
 class Controller extends CController
