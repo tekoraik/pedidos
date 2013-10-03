d0ba37a3d270fa53b5269d7430b83f34faadc8a0
tekoraik <luis@livetin.com>
2013-10-03 23:44:33 +0200

    TestTest

---
+++ protected/tests/unit/TestTest.php
@@ -0,0 +1,18 @@
+<?php
+class TestTest extends CDbTestCase
+{
+    
+ 
+ 	public function setUp() {
+ 		
+ 	}
+ 	
+ 	public function tearDown() {
+ 		
+ 	}
+ 	
+    public function testTesting() {
+    	$this->assertTrue(true);
+    }
+}
+?>
