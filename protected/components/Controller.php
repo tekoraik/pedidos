a266a79e22a39db483dccace87fc6d65f9d641ab
tekoraik <infoluis85@gmail.com>
2013-10-03 17:36:09 +0200

    All added?

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Controller is the customized base controller class
- * All controller classes for this application should extend from this base class.
+ * All controller classes for this application should extend from this base class
  */
 class Controller extends CController
 {
